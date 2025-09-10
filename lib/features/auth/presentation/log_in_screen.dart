95088bc67484c1368808dcfe07cf437ce2021b98
Talha Ismail <talha.ismail2119@gmail.com>
2025-09-10 14:02:48 +0600

    WIP - log in screen : core UI

--- lib/features/auth/presentation/log_in_screen.dart
+++ lib/features/auth/presentation/log_in_screen.dart
@@ -1,5 +1,10 @@
 
 import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
+import 'package:country_picker/country_picker.dart';
+import 'package:whats_up/common/widgets/custom_button_widgets.dart';
+
+import '../../../common/utils/colors.dart';
 class LogInScreen extends StatefulWidget {
   static const routeName = '/login-screen';
   const LogInScreen({super.key});
@@ -9,13 +14,77 @@ class LogInScreen extends StatefulWidget {
 }
 
 class _LogInScreenState extends State<LogInScreen> {
+  final phoneController = TextEditingController();
+  Country? country;
+
+
+  @override
+  void dispose() {
+    super.dispose();
+    phoneController.dispose();
+  }
+
+  void pickCountry() {
+    showCountryPicker(
+        context: context,
+        onSelect: (Country _country) {
+          setState(() {
+            country = _country;
+          });
+        });
+  }
+
   @override
   Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
     return Scaffold(
-      body: Center(
-        child: Text('This is Log in Screen'
+      appBar: AppBar(
+        title: const Text('Enter your phone number'),
+        elevation: 0,
+        backgroundColor: backgroundColor,
+      ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(18.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              const Text('WhatsApp will need to verify your phone number.'),
+              Gap(10),
+              TextButton(
+                onPressed: pickCountry,
+                child: const Text('Pick Country'),
+              ),
+              Gap(5),
+              Row(
+                children: [
+                  if (country != null) Text('+${country!.phoneCode}'),
+                  const Gap(10),
+                  SizedBox(
+                    width: size.width * 0.7,
+                    child: TextField(
+                      controller: phoneController,
+                      decoration: const InputDecoration(
+                        hintText: 'phone number',
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              SizedBox(height: size.height * 0.6),
+              SizedBox(
+                width: 90,
+                child: CustomButtonWidget(
+                  onPressed: (){},
+                  text: 'NEXT',
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
   }
 }
+
+

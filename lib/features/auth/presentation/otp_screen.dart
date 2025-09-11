39d57265d7518133bbdf0f10bf91eed73e779d52
Talha Ismail <talha.ismail2119@gmail.com>
2025-09-11 13:08:05 +0600

    otp screen - core UI

--- lib/features/auth/presentation/otp_screen.dart
+++ lib/features/auth/presentation/otp_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:gap/gap.dart';
 
+import '../../../common/utils/colors.dart';
+
 class OtpScreen extends StatefulWidget {
   static const routeName = '/otp-screen';
   final String verificationId;
@@ -14,6 +16,37 @@ class OtpScreen extends StatefulWidget {
 class _OtpScreenState extends State<OtpScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    final size = MediaQuery.of(context).size;
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Verifying your number'),
+        elevation: 0,
+        backgroundColor: backgroundColor,
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            const SizedBox(height: 20),
+            const Text('We have sent an SMS with a code.'),
+            SizedBox(
+              width: size.width * 0.5,
+              child: TextField(
+                textAlign: TextAlign.center,
+                decoration: const InputDecoration(
+                  hintText: '- - - - - -',
+                  hintStyle: TextStyle(
+                    fontSize: 30,
+                  ),
+                ),
+                keyboardType: TextInputType.number,
+                onChanged: (val) {
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }

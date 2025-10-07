ca997a6d3431aa9603b4ef52b0e32f41218e4cf9
Talha Ismail <talha.ismail2119@gmail.com>
2025-10-07 13:10:52 +0600

    WIP - bottom_chat_field

---
+++ lib/features/chat/repositories/widgets/bottom_chat_field_widget.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+
+import '../../../../common/utils/colors.dart';
+
+class BottomChatField extends StatelessWidget {
+  const BottomChatField({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Expanded(
+          child: TextField(
+            decoration: InputDecoration(
+              filled: true,
+              fillColor: mobileChatBoxColor,
+              prefixIcon: const Padding(
+                padding: EdgeInsets.symmetric(horizontal: 20.0),
+                child: Icon(Icons.emoji_emotions, color: Colors.grey),
+              ),
+              suffixIcon: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: const [
+                    Icon(Icons.camera_alt, color: Colors.grey),
+                    Icon(Icons.attach_file, color: Colors.grey),
+                    Icon(Icons.money, color: Colors.grey),
+                  ],
+                ),
+              ),
+              hintText: 'Type a message!',
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(20.0),
+                borderSide: const BorderSide(width: 0, style: BorderStyle.none),
+              ),
+              contentPadding: const EdgeInsets.all(10),
+            ),
+          ),
+        ), Padding(
+          padding: const EdgeInsets.only(
+            bottom: 8,
+            right: 2,
+            left: 2,
+          ),
+          child: CircleAvatar(
+            backgroundColor: const Color(0xFF128C7E),
+            radius: 25,
+            child: GestureDetector(
+              child: Icon(
+                    Icons.send,
+                color: Colors.white,
+              ),
+              //onTap: sendTextMessage,
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
